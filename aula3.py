ab60d83ae6cafcd16897a4de83a061af8fe5887b
Samuel Lopes Flores Silva <samuelaluno07if@gmail.com>
2025-07-25 19:07:51 -0300

    Add files via upload

--- aula3.py
+++ aula3.py
@@ -22,29 +22,6 @@ def calculadora():
         print('A divisão é : ', div)
 calculadora()
 
-print('Oi essa é a sua calculadora ')
-
-num_1 = float(input('Por favor, digite um numero: '))
-escolha = input('Digite sua operação: (+), (-), (*), (/) ')
-num_2 = float(input('Por favor, digite outro numero: '))
-
-if escolha == '+':
-    soma = num_1 + num_2
-    print('A soma é: ', soma)
-
-elif escolha == '-':
-    sub = num_1 - num_2
-    print('A subtração é: ', sub)
-
-elif escolha == '*':
-    mult = num_1 * num_2
-    print('A multiplicação é: ', mult)
-else:
-    div = num_1 / num_2
-    print('A divisão é : ', div)
-
-
-  
 
 #---- exercício ---
 
